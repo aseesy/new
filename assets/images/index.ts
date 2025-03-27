e1921871bf6f733e420d0ff2a69c5f20b390e6b5
aseesy <athenasees@gmail.com>
2025-03-27 13:57:13 -0700

    Add files via upload

---
+++ assets/images/index.ts
@@ -0,0 +1,20 @@
+import LiazenGlow from './SVGs/LogoGlow.svg';
+import LiazenDark from './SVGs/Logo.svg';
+import Header from './SVGs/Header.svg';
+
+export const IMAGES = {
+    effectiveCommunication: require('./effective-communication.png'),
+    userPrivacy: require('./user-privacy.png'),
+    sharedCalandar: require('./shared-calandar.png'),
+    logo4x: require('./logo-4x.png'),
+    logo4xGlow: require('./logo-4x-glow.png'),
+    header: require('./Vector.png'),
+    logoText: require('./logo-text.png'),
+    grid: require('./grid.png'),
+}
+
+export const SVGS = {
+    LiazenGlow,
+    LiazenDark,
+    Header
+}
